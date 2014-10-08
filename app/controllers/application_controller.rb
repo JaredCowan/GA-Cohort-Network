dfb7a275888093181d4d4adb2fcf0fda6e59cc51
JaredCowan <jaredlc@outlook.com>
2014-10-08 07:18:50 -0700

    added routing error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
 
+  rescue_from ActionController::RoutingError, :with => :render_not_found
   protect_from_forgery with: :exception
-
   before_action :update_time
+  include SessionsHelper
 
   def update_time
     if current_user
@@ -14,17 +15,12 @@ class ApplicationController < ActionController::Base
     true
     # current_user
   end
+  
+  def routing_error
+    raise ActionController::RoutingError.new(params[:path])
+  end
 
-  rescue_from ActionController::RoutingError, :with => :render_not_found
-
-def routing_error
-  raise ActionController::RoutingError.new(params[:path])
+  def render_not_found
+    render "public/404"
+  end
 end
-
-def render_not_found
-  render "public/404"
-end
-
-  include SessionsHelper
-
-end # End controller
