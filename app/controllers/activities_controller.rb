882ddd2979495e5db7ac31f6e96b3f5c0b086eb8
JaredCowan <jaredlc@outlook.com>
2014-10-27 17:50:33 -0700

    Styled and working on layout of activities feed

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,5 @@
 class ActivitiesController < ApplicationController
-  # respond_to :html, :json
   def index
-    @activities = Activity.all.reverse
+    @activities = Activity.all.order("created_at DESC")
   end
 end
