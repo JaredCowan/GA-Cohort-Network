7f969810bbbc5ad8c750fb3938dfe55490fbf517
JaredCowan <jaredlc@outlook.com>
2015-02-21 22:06:40 -0800

    update URL

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -1,7 +1,7 @@
 class HookController < ApplicationController
   def comment
     usr = {:username => ENV['GH_VALUE']}
-    HTTParty.post(URL, :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GA_TOKEN'] }, :body => { :body => "testing thiissss"}.to_json) 
+    HTTParty.post(ENV['URL'], :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GA_TOKEN'] }, :body => { :body => "testing thiissss"}.to_json) 
     # puts "status code: #{post.code}, status message: #{post.message}"
     # puts pretty(body)
   end
