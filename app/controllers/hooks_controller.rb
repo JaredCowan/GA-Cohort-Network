1fd8797c243610caa84214b70313f536c9b6adf9
JaredCowan <jaredlc@outlook.com>
2015-02-22 02:01:36 -0800

    test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -5,8 +5,9 @@ class HooksController < ApplicationController
   def post_callback
     request.body.rewind
     pay = JSON.parse(request.body.read)
-    # puts pay['action']
+    action = "#{pay['action']}"
 
+    action == "created"
     usr = {:username => ENV['GH_VALUE']}
     issuebody = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
     post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => issuebody}.to_json) 
