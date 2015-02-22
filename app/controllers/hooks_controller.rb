83b044a35a53e890481656f76c6d5a33f7e791a0
JaredCowan <jaredlc@outlook.com>
2015-02-22 01:58:26 -0800

    test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -5,23 +5,7 @@ class HooksController < ApplicationController
   def post_callback
     request.body.rewind
     pay = JSON.parse(request.body.read)
-    puts pay['action']
-
-    pay['action'] == created
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
-    pay['action'] == "created"
+    # puts pay['action']
 
     usr = {:username => ENV['GH_VALUE']}
     issuebody = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
