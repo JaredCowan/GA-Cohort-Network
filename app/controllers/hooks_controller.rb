b4011d8fcfb0fb71f6aa2144b94fd9272a1e2973
JaredCowan <jaredlc@outlook.com>
2015-02-22 04:22:15 -0800

    testt

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -18,15 +18,18 @@ class HooksController < ApplicationController
     # end
 
     # if action == "opened"
-      usr = {:username => name}
-      # issuebody = payload['issue']['body']
+      usr = {:username => "#{name}"}
+      issuebody = payload['issue']['body']
       addon = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
 
-      final = "@JaredCowan you have a new issue."
+      final = "#{issuebody} \r\n #{addon} \r\n @JaredCowan you have a new issue."
 
-      HTTParty.post("https://api.github.com/repos/JaredCowan/GA-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => "#{usr[:username]}", "Authorization" => "#{token}" }, :body => { :body => "final"}.to_json) 
+      # post = HTTParty.POST("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => usr[:username], "Authorization" => "#{token}" }, :body => { :body => final}.to_json) 
+      post = HTTParty.post("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => "VerdeCircle-Bot", "Authorization" => "token 809094b8331d415d955bb191f0dae93ab13c10ec" }, :body => { :body => "winning"}.to_json)
+
+      puts post
     # end
 
-    # render :nothing => true
+    render :nothing => true
   end
 end
