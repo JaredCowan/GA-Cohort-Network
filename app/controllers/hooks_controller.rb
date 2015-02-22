20530361dbb792c0e63d8d03c07423f6197a4479
JaredCowan <jaredlc@outlook.com>
2015-02-21 23:24:46 -0800

    test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,11 +4,12 @@ class HooksController < ApplicationController
 
   def post_callback
     usr = {:username => ENV['GH_VALUE']}
-    post = HTTParty.post("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => "catttssss"}.to_json) 
+    post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => "catttssss"}.to_json) 
     # puts "status code: #{post.code}, status message: #{post.message}"
     # puts pretty(body)
     response.body = "200"
     puts request.inspect
+    puts post
     render :nothing => true
   end
 end
