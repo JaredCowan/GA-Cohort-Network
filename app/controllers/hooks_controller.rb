2a0539511fdcab248275d8bc33e16ad357d2a4cf
JaredCowan <jaredlc@outlook.com>
2015-02-22 02:15:43 -0800

    test

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,13 +4,27 @@ class HooksController < ApplicationController
 
   def post_callback
     request.body.rewind
-    pay = JSON.parse(request.body.read)
-    action = "#{pay['action']}"
+    payload = JSON.parse(request.body.read)
+    action = "#{payload['action']}"
 
-    puts action == "created"
-    usr = {:username => ENV['GH_VALUE']}
-    issuebody = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
-    post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => issuebody}.to_json) 
+    if action == "created"
+      comment = payload['comment']
+
+
+      usr = {:username => ENV['GH_VALUE']}
+      issuebody = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
+      post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => issuebody}.to_json) 
+    end
+
+    if action == "opened"
+      usr = {:username => ENV['GH_VALUE']}
+      issuebody = payload['issue']['body']
+      addon = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
+
+      final = "#{issuebody} \r\n #{addon} \r\n @JaredCowan you have a new issue."
+
+      post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => issuebody}.to_json) 
+    end
 
     render :nothing => true
   end
