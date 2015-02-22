0ffaac73a4cfeef13a7835025f449eb8bf2171de
JaredCowan <jaredlc@outlook.com>
2015-02-22 04:51:06 -0800

    bot working

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -6,8 +6,8 @@ class HooksController < ApplicationController
     request.body.rewind
     payload = JSON.parse(request.body.read)
     action = "#{payload['action']}"
-    name = ENV['verdebotname']
-    token = ENV['verdebottoken']
+    $name = ENV['verdebotname']
+    $token = ENV['verdebottoken']
     $bot = "#{payload['comment']['user']['login']}"
     # if action == "created"
     #   comment = payload['comment']
@@ -19,14 +19,15 @@ class HooksController < ApplicationController
     # end
     puts "#{$bot}" == "VerdeCircle-Bot"
     if $bot != "VerdeCircle-Bot"
-      usr = {:username => "#{name}"}
+      usr = {:username => "#{$name}"}
       issuebody = payload['issue']['body']
       addon = %(<a href="#"><img src="http://www.codereviewhub.com/site/github-approved-avatar.png" align="left" height="34" width="246"><img src='https://avatars.githubusercontent.com/u/7110664?v=3' width=34 height=34></a>) 
 
-      final = "#{issuebody} \r\n #{addon} \r\n @JaredCowan you have a new issue."
+      final = "#{issuebody} \r\n #{addon} \r\n @JaredCowan you have a new issue." <<
+      "This is a fully automated comment."
 
       # post = HTTParty.POST("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => usr[:username], "Authorization" => "#{token}" }, :body => { :body => final}.to_json) 
-      post = HTTParty.post("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => "VerdeCircle-Bot", "Authorization" => "token 809094b8331d415d955bb191f0dae93ab13c10ec" }, :body => { :body => "#{final}"}.to_json)
+      post = HTTParty.post("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1/comments", :headers => { "User-Agent" => "#{$name}", "Authorization" => "#{$token}" }, :body => { :body => "#{final}"}.to_json)
 
       puts payload['comment']['user']['login']
     end
