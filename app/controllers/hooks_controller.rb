07cefc7b45b0cc2a7b536a5cf0e11caae023a6a6
JaredCowan <jaredlc@outlook.com>
2015-02-21 23:39:27 -0800

    holy fucking shit it works

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -7,8 +7,7 @@ class HooksController < ApplicationController
     post = HTTParty.patch("https://api.github.com/repos/JaredCowan/Ga-Cohort-Network/issues/1", :headers => { "User-Agent" => usr[:username], "Authorization" => ENV['GH_TOKEN'] }, :body => { :body => "catttssss"}.to_json) 
     # puts "status code: #{post.code}, status message: #{post.message}"
     # puts pretty(body)
-    response.body = "200"
-    puts request.inspect
+    # puts request.inspect
     puts post
     render :nothing => true
   end
